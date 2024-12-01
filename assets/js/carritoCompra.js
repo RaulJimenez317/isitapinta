2b7f96bc6c3164fdfd223af971b5bf87c270a10f
RaulJimenez317 <jcr0034569@est.univalle.edu>
2024-12-01 19:00:51 -0400

    Arreglo de detalles finales

--- assets/js/carritoCompra.js
+++ assets/js/carritoCompra.js
@@ -16,7 +16,7 @@ botonesAgregar.forEach((boton) => {
   boton.addEventListener('click', () => {
     const producto = boton.parentElement.querySelector('h1').textContent;
     const precio = parseFloat(
-      boton.parentElement.querySelector('h2').textContent.replace('$', '')
+      boton.parentElement.querySelector('h2').textContent.replace('Bs', '')
     );
 
     const productoExistente = carrito.find((item) => item.producto === producto);
@@ -39,14 +39,14 @@ function actualizarCarrito() {
     const li = document.createElement('li');
     li.innerHTML = `
       <span>${item.producto} x ${item.cantidad}</span>
-      <span>$${(item.precio * item.cantidad).toFixed(2)}</span>
+      <span>Bs${(item.precio * item.cantidad).toFixed(2)}</span>
       <button data-index="${index}" class="eliminar-producto">X</button>
     `;
     listaCarrito.appendChild(li);
     total += item.precio * item.cantidad;
   });
 
-  totalElement.textContent = `Total: $${total.toFixed(2)}`;
+  totalElement.textContent = `Total: Bs${total.toFixed(2)}`;
   guardarCarritoEnLocalStorage();
   agregarEventosEliminar();
 }
